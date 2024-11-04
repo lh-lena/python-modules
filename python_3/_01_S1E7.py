79c20bc759a871de1699aeb8a0ff0137cfcb5044
Olena Hladkovska <ohladkov@student.42berlin.de>
2024-11-04 15:36:34 +0100

    py03/ex01 implemented derived classes

--- python_3/_01_S1E7.py
+++ python_3/_01_S1E7.py
@@ -1,10 +1,11 @@
 """
 Create two families that inherit from the Character class, that we can instantiate
 without going through the Character class. Find a solution so that "__str__" and
-"__repr__" return strings and not objects. Write a Class method to create characters
-in a chain.
+"__repr__" return strings and not objects.
+Write a Class method to create characters in a chain.
 
 https://python-course.eu/oop/object-oriented-programming.php
+https://docs.python.org/2/library/functions.html#super
 
 """
 
@@ -12,11 +13,43 @@ from _00_S1E9 import Character
 
 class Baratheon(Character):
     #your code here
+    """Representing the Baratheon family."""
+    def __init__(self, firt_name: str):
+        self.family_name = 'Baratheon'
+        self.eyes = 'brown'
+        self.hairs = 'dark'
+        super().__init__(firt_name)
 
+    def __str__(self):
+        return f"{type(self)} ('{self.family_name}, {self.eyes}, {self.hairs})"
 
+    def __repr__(self):
+        return f"{type(self)} ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    def die(self):
+        if self.is_alive:
+            self.is_alive = False
+        return self.is_alive
 
 class Lannister(Character):
     #your code here
-        
+    """Representing the Lannister family."""
+
+    def __init__(self, firt_name: str):
+        self.family_name = 'Lannister'
+        self.eyes = 'blue'
+        self.hairs = 'light'
+        super().__init__(firt_name)
+
+    def __str__(self):
+        return f"('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    def __repl__(self):
+        return f"('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
         # decorator
-        def create_lannister(your code here):
+        # def create_lannister(your code here):
+    def die(self):
+        if self.is_alive:
+            self.is_alive = False
+        return self.is_alive
