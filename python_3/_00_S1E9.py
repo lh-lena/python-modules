27d2e8c58f9b8518ff2c5054451d8a2e788010c0
Olena Hladkovska <ohladkov@student.42berlin.de>
2024-11-04 11:51:53 +0100

    py03/ex00 implemented ABCs

--- python_3/_00_S1E9.py
+++ python_3/_00_S1E9.py
@@ -5,3 +5,29 @@ health state of the character with a method that passes is_alive from True to Fa
 And a "stark" class which inherits from Character
 """
 
+# Python program showing
+# abstract base class work
+from abc import ABC, abstractmethod
+
+class Character(ABC):
+    """Your docstring for Class"""
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """Your docstring for Constructor"""
+        self.first_name = first_name
+        self.is_alive = is_alive
+        super().__init__()
+
+    @abstractmethod
+    def die(self):
+        """Your docstring for Method"""
+        pass
+
+
+class Stark(Character):
+    """Your docstring for Class"""
+    def die(self) -> bool:
+        """Your docstring for Method"""
+        if self.is_alive == True:
+            self.is_alive = False
+        return self.is_alive
+
