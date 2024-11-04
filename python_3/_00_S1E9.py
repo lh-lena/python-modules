9b509420c45d510ef8cd600af359419204f690ab
Lena <hladkovskaa@gmail.com>
2024-11-04 09:30:19 +0100

    py03 init

---
+++ python_3/_00_S1E9.py
@@ -0,0 +1,7 @@
+"""_summary_
+Create an abstract class "character" which can take a first_name as first parameter,
+is_alive as second non mandatory parameter set to True by default and can change the
+health state of the character with a method that passes is_alive from True to False.
+And a "stark" class which inherits from Character
+"""
+
---
+++ python_3/_00_tester.py
@@ -0,0 +1,34 @@
+from _00_S1E9 import Character, Stark
+Ned = Stark("Ned")
+print(Ned.__dict__)
+print(Ned.is_alive)
+Ned.die()
+print(Ned.is_alive)
+print(Ned.__doc__)
+print(Ned.__init__.__doc__)
+print(Ned.die.__doc__)
+print("---")
+Lyanna = Stark("Lyanna", False)
+print(Lyanna.__dict__)
+
+"""
+Expected output: (docstrings can be different)
+$> python tester.py
+{'first_name': 'Ned', 'is_alive': True}
+True
+False
+Your docstring for Class
+Your docstring for Constructor
+Your docstring for Method
+---
+{'first_name': 'Lyanna', 'is_alive': False}
+$>
+"""
+
+"""
+should make an error
+
+hodor = Character("hodor")
+TypeError: Can't instantiate abstract class Character with abstract method
+$>
+"""
