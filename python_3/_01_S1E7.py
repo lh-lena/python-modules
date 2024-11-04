ccb7a9d0dff5fb88119f396b9ba4604aa23bcc4b
Olena Hladkovska <ohladkov@student.42berlin.de>
2024-11-04 11:56:50 +0100

    py03/ex01 init

---
+++ python_3/_01_S1E7.py
@@ -0,0 +1,22 @@
+"""
+Create two families that inherit from the Character class, that we can instantiate
+without going through the Character class. Find a solution so that "__str__" and
+"__repr__" return strings and not objects. Write a Class method to create characters
+in a chain.
+
+https://python-course.eu/oop/object-oriented-programming.php
+
+"""
+
+from _00_S1E9 import Character
+
+class Baratheon(Character):
+    #your code here
+
+
+
+class Lannister(Character):
+    #your code here
+        
+        # decorator
+        def create_lannister(your code here):
