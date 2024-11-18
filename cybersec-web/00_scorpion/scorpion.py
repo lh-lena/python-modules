6a75f3dc6474975faab574748abd7249ab5a2052
Olena Hladkovska <ohladkov@student.42berlin.de>
2024-11-18 12:32:53 +0100

    cebersec-web/d00/ex00: scorpion: nip

--- cybersec-web/00_scorpion/scorpion.py
+++ cybersec-web/00_scorpion/scorpion.py
@@ -1,13 +1,25 @@
 import exif
+import sys
+
+def main():
+    try:
+        ac = len(sys.argv)
+        assert ac >= 2, f"Missing file\nUsage: {sys.argv[0]} FILE1 [FILE2 ...]"
+        data = parseInput(sys.argv[1:])
+        scorpion(data)
+    except AssertionError as e:
+        print(f"{sys.argv[0]}: Error: {e}")
+        return -1
+    except Exception as e:
+        print(f"{sys.argv[0]}: Error: {e}")
+        return -1
 
 def parseInput(argv: str, extension=[".jpg", ".jpeg", ".png", ".gif", ".bmp"]) -> list:
     data = []
     for i in argv:
-        if not i or not any(i.lower().endswith(ext) for ext in extension):
-            raise Exception("scorpion: Unknown file format")
-        data.append(i)
-    if not data:
-        raise Exception("scorpion: Missing file\nUsage: main.py FILE1 [FILE2 ...]")
+        if i and any(i.lower().endswith(ext) for ext in extension):
+            data.append(i)
+    assert data, "Missing file\nUsage: main.py FILE1 [FILE2 ...]"
     return data
 
 def scorpion(data: list):
@@ -15,7 +27,8 @@ def scorpion(data: list):
         try:
             with open(file, "rb") as f:
                 image = exif.Image(f)
-                tags = sorted(image.list_all())
+                # tags = sorted(image.list_all())
+                tags = image.list_all()
                 if not tags:
                     print(f"File {file}: No EXIF data found.")
                     continue
@@ -24,7 +37,9 @@ def scorpion(data: list):
                 if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
                     print("%s: %s" % (tag, image.get(tag)))
         except FileNotFoundError:
-            print(f"Error: File {file} not found.")
+            print(f"File {file} not found.")
         except Exception as e:
             print(f"An error occurred while processing {file}: {e}")
 
+if __name__ == "__main__":
+    main()
