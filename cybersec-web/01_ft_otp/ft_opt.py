08f8767167fcb33dfefd313b38350fd465e7fc0b
Olena Hladkovska <ohladkov@student.42berlin.de>
2024-11-23 22:04:26 +0100

    cybersecurity/ex01/ft_opt: ft_hopt: to fix -> ./ft_opt.py: error: cannot convert 'int' object to bytes.

--- cybersec-web/01_ft_otp/ft_opt.py
+++ cybersec-web/01_ft_otp/ft_opt.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 import string
 import time
+import datetime
 import hashlib
 import hmac
 from ft_hmac import ft_hmac
@@ -10,6 +11,7 @@ from cryptography.fernet import Fernet
 
 KEY = "68656c6c6f68656c6c6f68656c6c6f68656c6c6f"
 MSG = "68656c6c6f68656c6c6f68656c6c6f68656c6c6f"
+TIME_DURATION = 30
 
 def ft_opt():
     try:
@@ -22,12 +24,16 @@ def ft_opt():
         args = parser.parse_args()
         assert ac == 3, f"Invalid input\nUsage: {sys.argv[0]} [OPTION] [KEY_FILE]\n-g:  to save encrypted key given as argument to file.\n-k:  to generate a new temporary password based on the key given as argument"
 
+        cur_time = int(time.time())
+        T = (cur_time - 1732289019) // TIME_DURATION
         if args.encrypt:
             encrypt(args.key)
         elif args.generate:
-            print(ft_hmac(KEY.encode(), MSG.encode()))
-            h = hmac.new(KEY.encode(), MSG.encode(), hashlib.sha1)
+            HS = ft_hmac(KEY.encode(), str(T).encode())
+            print(HS)
+            h = hmac.new(KEY.encode(), str(T).encode(), hashlib.sha1)
             print("Org HMAC:\n",h.digest())
+            print(ft_hopt(HS))
     except AssertionError as e:
         print(f"{sys.argv[0]}: error: {e}.")
         return 0
@@ -53,25 +59,40 @@ def ft_hopt(HS: bytes):
 
     Args:
         HS (bytes): Generated an HMAC-SHA-1 value, a 20-byte string 
+    Returns:
+        int: A 6-digit calculated HOTP value from the HMAC value
     """
+    
+    def strToNum(string: bytes) -> int:
+        return int.from_bytes(string, byteorder='big')
+
+    def DT(string: bytes) -> str:
+        """Perfoms the dynamic truncation and then the reduction modulo 10^6
+        The purpose of the dynamic offset truncation technique is 
+        to extract a 4-byte dynamic binary code from a 160-bit (20-byte) 
+        HMAC-SHA-1 result.
+
+        Returns:
+            int: Return the Last 31 bits of p
+        """
+        print("strToNum(string[19]", strToNum(string[19]))
+        offset = strToNum(string[19]) & 0xf # 0 <= offset <= 15
+        print("offset  ", offset)
+        
+        # p_str = string[offset:offset+4]
+        # p = strToNum(p_str) & 0x7fffffff
+        p = ((string[offset] & 0x7f) << 24 | 
+            (string[offset+1] & 0xff) << 16 | 
+            (string[offset+2] & 0xff) << 8 | 
+            (string[offset+3] & 0xff))
+        return p
+        
     # Generate a 4-byte string (Dynamic Truncation)
     Sbits = DT(HS)
     # Compute an HOTP value
     Snum = strToNum(Sbits)
-    D = Snum % 10**6
-    
-    def DT(string: bytes):
-        offsetBits = string[19]
-        offset = strToNum(offsetBits) # 0 <= offset <= 15
-        '''
-        Let P = String[OffSet]...String[OffSet+3]
-        Return the Last 31 bits of P
-        '''
-        
-        
-    def strToNum(string):
-        return int(string, 2)
-    
+    D = Snum % 10 ** 6
     return D
+
 if __name__ == "__main__":
     ft_opt()
