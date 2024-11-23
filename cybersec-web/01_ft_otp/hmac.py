41f728d24e7731e7e2edcaba86a5fdfdff81b704
Lena <hladkovskaa@gmail.com>
2024-11-23 13:45:57 +0100

    cybersecurity/ex01/ft_opt: init

--- cybersec-web/01_ft_otp/hmac.py
+++ cybersec-web/01_ft_otp/hmac.py
@@ -1,46 +1,38 @@
+#!/usr/bin/env python3
 import hashlib
 
-def HMAC(key: str, m: str, hash, l=20, block_size=64):
+KEY = "x347137324A48675838397A33426B464D7436637751784C31724432386A704E3555665668495A59506243536575476F765261576D413073443945437458374A66"
+MSG = "x48656C6C6F"
+
+def HMAC(key: str, msg: str, hash, l=20, block_size=64):
     """
     to hash msg
     :param str Key: Authentication key
-    :param str m: Is the message to be authenticated
+    :param str msg: Is the message to be authenticated
     :param func hash: The hash function to use (e.g. SHA-1)
     :param int l: output size of hash function (e.g. 20 bytes for SHA-1)
     :param int block_size: The block size of the hash function (e.g. 64 bytes for SHA-1)
     """
-    block_sized_key = computeBlockSizedKey(key, hash, block_size)
 
-    #start out by storing key in pads
-    bzero( k_ipad, sizeof k_ipad)
-    bzero( k_opad, sizeof k_opad)
-    bcopy( key, k_ipad, key_len)
-    bcopy( key, k_opad, key_len)
-    # ipad = 
+    if len(key) < block_size:
+        key = key.encode() + b'\x00' * (block_size - len(key))
 
-    return True
-
-def computeBlockSizedKey(key: str, hash, block_size: int):
-    """
-    Computes the block sized key
-
-    :param str Key: Authentication key
-    :param func hash: The hash function to use (e.g. SHA-1)
-    :param int block_size: The block size of the hash function (e.g. 64 bytes for SHA-1)
-    """
     # Keys longer than block_size are shortened by hashing them
     if len(key) > block_size:
-        key = hash(key)
+        key = hash(key.encode()).digest()
 
     # Keys shorter than block_size are padded to block_size by padding with zeros on the right
-    if len(key) < block_size:
-        ss = block_size - len(key)
-        key = key + '0' * ss
+    ipad = b'\x36' * block_size
+    opad = b'\x5C' * block_size
+    k_ipad = key.digest() ^ ipad
+    k_opad = key.digest() ^ opad
+    msg_padded = k_ipad + msg
+    hashed_msg = hash(msg_padded.encode())
+    conct_res = k_opad + hashed_msg
 
-    print(key)
-    return key
-    
-computeBlockSizedKey("hello", hashlib.sha1, 64)
+    return hash(conct_res.encode())
+
+print(HMAC(KEY, MSG, hashlib.sha1, 20, 64))
 
 #HMAC_SHA1("key", "The quick brown fox jumps over the lazy dog")   = de7c9b85b8b78aa6bc8a7a36f70a90701c9db4d9
 
