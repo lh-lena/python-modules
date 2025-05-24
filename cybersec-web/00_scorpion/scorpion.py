44e77eb720ac2a7ca5fecdeb39c901fe077689b9
Olena Hladkovska <ohladkov@student.42berlin.de>
2025-05-24 14:47:13 +0200

    finished

--- cybersec-web/00_scorpion/scorpion.py
+++ cybersec-web/00_scorpion/scorpion.py
@@ -1,88 +1,191 @@
 #!/usr/bin/env python3
-import sys
 import os
-from datetime import datetime
-from PIL import Image, UnidentifiedImageError
-from PIL.ExifTags import TAGS
-import chardet
+import re
+import requests
+import argparse
+from colorama import Fore, Style
+from urllib import parse
+from bs4 import BeautifulSoup as BS
 
-''' to run with all .jpg images from data directory
-find data -name "*.jpg" | xargs ./scorpion.py
-'''
+tags_attributes = {
+    'img': ['src', 'srcset'],
+    'image': ['href'],
+    'source': ['src', 'srcset'],
+    'a': ['href'],
+    'link': ['href'],
+    'div': ['style'],
+    'video': ['poster', 'srs'],
+	'audio': ['src'],
+    'embed': ['src'],
+    'object': ['data'],
+    'background': ['url'],
+    'background-image': ['url'],
+    'svg': ['href'],
+    'span': ['style'],
+    'p': ['style'],
+    'html': ['style']
+}
 
-def scorpion():
+pattern = r'(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])(?!(?:\.xml)(?=\s|$))'
+
+def spider():
     try:
-        ac = len(sys.argv)
-        assert ac >= 2, f"Missing file\nUsage: {sys.argv[0]} FILE1 [FILE2 ...]"
-        data = parseInput(sys.argv[1:])
-        for file in data:
-            try:
-                image = Image.open(file)
-                get_basic_metadata(image)
-                get_data_attr(file)
-                get_exifdata(image)
-            except UnidentifiedImageError as e:
-                print(f"{sys.argv[0]}: Error: {e}")
-                continue
+        parser = argparse.ArgumentParser(
+                    prog='spider',
+                    description='Image scraping')
+        
+        parser.add_argument('url', help='URL to a website')
+        parser.add_argument('-r', '--recursive', action='store_true', help='Recursively download images')
+        parser.add_argument('-l', '--level', type=int, default=5, help='Maximum recursion depth')
+        parser.add_argument('-p', '--path', default='./data', help='Download path')
+
+        args = parser.parse_args()
+        if args.level < 0:
+            raise argparse.ArgumentTypeError(f"{args.level} must be a positive integer")
+        assert args.url, f"Invalid URL {args.url}"
+        assert args.path, f"Invalid path {args.path}"
+        print_colored_flags("[INFO]    ", "Initial data", Fore.BLUE)
+        log(args.url, args.recursive, 0, args.level)
+        print_colored_flags("[INFO]    ", f"Path: {os.path.abspath(args.path)}\n", Fore.BLUE)
+        visited_urls = set()
+        scrapePage(args.url, args.path, 0, args.level, args.recursive, visited_urls)
+
     except AssertionError as e:
-        print(f"{sys.argv[0]}: Error: {e}")
-        return -1
+        print_colored_flags("[ERROR]   ", e, Fore.RED)
+        return 1
+    except ValueError as e:
+        print_colored_flags("[ERROR]   ", e, Fore.RED)
+        return 2
+    except TypeError as e:
+        print_colored_flags("[ERROR]   ", e, Fore.RED)
+        return 3
+    except IsADirectoryError as e:
+        print_colored_flags("[ERROR]   ", e, Fore.RED)
+        return 4
     except Exception as e:
-        print(f"{sys.argv[0]}: Error: {e}")
-        return -1
+        print_colored_flags("[ERROR]   ", e, Fore.RED)
+        return 5
+    except KeyboardInterrupt:
+        print()
+        return 6
 
-def get_data_attr(image_path):
-    file_stat = os.stat(image_path)
-    creation_time = datetime.fromtimestamp(file_stat.st_ctime).strftime('%Y-%m-%d %H:%M:%S')
+def log(base_url: str, recursive: bool, depth: int, maxDepth: int):
+    print_colored_flags("[INFO]    ", f"Processing: {base_url}", Fore.BLUE)
+    print_colored_flags("[INFO]    ", f"Recursive: {recursive}", Fore.BLUE)
+    print_colored_flags("[INFO]    ", f"Depth level: {depth}/{maxDepth}", Fore.BLUE)
 
-    print(f"Creation Date            : {creation_time}")
+def scrapePage(base_url: str, pathDir: str, depth: int, maxDepth: int, recursion: bool, visited_urls: list, extension=[".jpg", ".jpeg", ".png", ".gif", ".bmp"]):
+    if base_url in visited_urls:
+        return
+    log(base_url, recursion, depth, maxDepth)
+    if depth >= maxDepth:
+        print_colored_flags("[WARNING] ", "Maximum recursion depth is reached and no image is found", Fore.YELLOW)
+        return
 
-def parseInput(argv: str, extension=[".jpg", ".jpeg", ".png", ".gif", ".bmp"]) -> list:
-    data = []
-    for i in argv:
-        if i and any(i.lower().endswith(ext) for ext in extension):
-            data.append(i)
-    assert data, "Missing file\nUsage: scorpion.py FILE1 [FILE2 ...]"
-    return data
+    visited_urls.add(base_url)
 
-def get_basic_metadata(image):
-    info_dict = {
-        "Filename": image.filename,
-        "Image Size": image.size,
-        "Image Height": image.height,
-        "Image Width": image.width,
-        "Image Format": image.format,
-        "Image Mode": image.mode,
-        "Image is Animated": getattr(image, "is_animated", False),
-        "Frames in Image": getattr(image, "n_frames", 1)
-    }
-    print()
-    for label, value in info_dict.items():
-        print(f"{label:25}: {value}")
+    if isExtension(base_url, extension):
+        downloadImage(base_url, pathDir)
+        print(base_url, "\n")
+        return
+    
+    response = getResponse(base_url)
+    if not response:
+        return
 
-def get_exifdata(image):
-    exif_data = image.getexif()
-    if exif_data:
-        print("\nEXIF Metadata:")
-        for tag_id, value in exif_data.items():
-            tag_name = TAGS.get(tag_id, tag_id)
-            if isinstance(value, bytes):
-                value = decode_with_chardet(value)
-            print(f"{tag_name}: {value}")
-        creation_date = exif_data.get("DateTimeOriginal")
-        modification_date = exif_data.get("DateTime")
-        print(f"Creation Date           : {creation_date}")
-        print(f"Modification Date       : {modification_date}")
+    content = response.content
+    if not content or not recursion:
+        print_colored_flags("[WARNING] ", "Recursion, depth level are not given or no image is found", Fore.YELLOW)
+        return
 
+    if '<?xml' in content.decode(errors='ignore'):
+        soup = BS(content, "lxml-xml")
     else:
-        print("No EXIF metadata found\n")
+        soup = BS(content, 'html.parser')
+    urls = []
+    for tag, attrs in tags_attributes.items():
+        for elem in soup.find_all(tag):
+            for attr in attrs:
+                val = elem.get(attr)
+                if not val:
+                    continue
+                if attr == 'style':
+                    srcs = re.findall(r'url\(["\']?(.*?\.(?:jpg|jpeg|png|gif|bmp))["\']?\)', val)
+                    for src in srcs:
+                        if isRelative(src) and not '#' in src and not src.endswith('='):
+                            urls.append(parse.urljoin(base_url, src))
+                        else:
+                            urls.append(src)
+                else:
+                    if isRelative(val):
+                        urls.append(parse.urljoin(base_url, val))
+                    else:
+                        urls.append(val)
+    try:
+        regex_urls = re.findall(pattern, content.decode())
+        for url in regex_urls:
+            full_url = url[0] + '://' + url[1] + url[2]
+            if not '#' in full_url and not full_url.endswith('='):
+                urls.append(full_url)
+    except UnicodeDecodeError:
+        pass
+    urls = list(set(urls))
+    for url in urls:
+        if isExtension(url, extension):
+            log(url, recursion, depth + 1, maxDepth)
+            downloadImage(url, pathDir)
+        else:
+            scrapePage(url, pathDir, depth + 1, maxDepth, recursion, visited_urls)
 
-def decode_with_chardet(value):
-    result = chardet.detect(value)
-    encoding = result['encoding']
-    if not encoding:
-        return ""
-    return value.decode(encoding, errors='replace')
+def downloadImage(imageUrl: str, pathDir: str):
+    """Creates directories based on the URL path and saves the image to the final directory"""
+    parsed_url = parse.urlparse(imageUrl)
+    path = pathDir + os.path.dirname(parsed_url.path)
+    filePath = os.path.join(path, os.path.basename(imageUrl))
+
+    os.makedirs(path, exist_ok=True)
+    if os.path.exists(filePath):
+        print_colored_flags("[WARNING] ", f"Image {imageUrl} already saved", Fore.YELLOW)
+        return
+
+    response = getResponse(imageUrl)
+    if not response:
+        return
+    file = open(filePath, "wb")
+    for chunk in response.iter_content(chunk_size=8192):
+        file.write(chunk)
+    file.close()
+    print_colored_flags("[SUCCESS] ", f"{imageUrl} saved", Fore.GREEN)
+    
+
+def getResponse(url: str):
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        return response
+    except requests.exceptions.ConnectTimeout as e:
+        print_colored_flags("[ERROR]   ", "{e}", Fore.RED)
+        return []
+    except requests.exceptions.RequestException as e:
+        print_colored_flags("[ERROR]   ", e, Fore.RED)
+        return []
+
+def isValidUrl(url):
+    parsed = parse.urlparse(url)
+    return bool(parsed.netloc) and bool(parsed.scheme)
+
+def isExtension(src: str, extensions: list)-> bool:
+    """Check if the URL ends with one of the specified extensions"""
+    return any(src.lower().endswith(ext) for ext in extensions) and not '#' in src
+
+def isRelative(url: str) -> bool:
+    """Check if a URL is relative"""
+    if not re.match(r'^(?:https?|ftp):', url):
+        return True
+    return False
+
+def print_colored_flags(flag, message, color):
+    print(f"{color}{flag:10}{Style.RESET_ALL}{message}")
 
 if __name__ == "__main__":
-    scorpion()
+    spider()
